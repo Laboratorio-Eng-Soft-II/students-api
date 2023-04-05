bd16a2e9f332406e9f2bda85b6b834cca1f06e12
GustavoBarros360 <gustavobarros2001@gmail.com>
2023-04-05 08:52:16 -0300

    add cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
-require('dotenv').config();
-import express, { Response } from 'express';
-import { Routes } from "./routes"
-import config from 'config';
-import validateEnv from './utils/validate-env';
-import { AppDataSource } from './utils/data-source';
+require("dotenv").config();
+import express, { Response } from "express";
+import { Routes } from "./routes";
+import config from "config";
+import validateEnv from "./utils/validate-env";
+import { AppDataSource } from "./utils/data-source";
+import cors from "cors";
 
 AppDataSource.initialize()
   .then(async () => {
@@ -22,25 +23,36 @@ AppDataSource.initialize()
     // 3. Cookie Parser
 
     // 4. Cors
+    app.use(cors());
 
     // ROUTES
-    Routes.forEach(route => {
-      (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
-        const result = (new (route.controller as any))[route.action](req, res, next)
-        if (result instanceof Promise) {
-          result.then(result => result !== null && result !== undefined ? res.send(result) : undefined)
-
-        } else if (result !== null && result !== undefined) {
-          res.json(result)
+    Routes.forEach((route) => {
+      (app as any)[route.method](
+        route.route,
+        (req: Request, res: Response, next: Function) => {
+          const result = new (route.controller as any)()[route.action](
+            req,
+            res,
+            next
+          );
+          if (result instanceof Promise) {
+            result.then((result) =>
+              result !== null && result !== undefined
+                ? res.send(result)
+                : undefined
+            );
+          } else if (result !== null && result !== undefined) {
+            res.json(result);
+          }
         }
-      })
-    })
+      );
+    });
 
     // HEALTH CHECKER
-    app.get('/api/healthchecker', async (_, res: Response) => {
+    app.get("/api/healthchecker", async (_, res: Response) => {
       res.status(200).json({
-        status: 'success',
-        message: 'healthy',
+        status: "success",
+        message: "healthy",
       });
     });
 
@@ -48,10 +60,9 @@ AppDataSource.initialize()
 
     // GLOBAL ERROR HANDLER
 
-    const port = config.get<number>('port');
+    const port = config.get<number>("port");
     app.listen(port);
 
     console.log(`Server started on port: ${port}`);
   })
   .catch((error) => console.log(error));
-
